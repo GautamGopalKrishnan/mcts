6a5475b3b4d4edde5852548f241650e33758793a
Dylan Peifer <djp282@cornell.edu>
2020-06-16 21:04:48 -0500

    Add check.pyx to cythonize list

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("mcts/tic.pyx"))
-setup(ext_modules=cythonize("mcts/connect.pyx"))
+setup(ext_modules=cythonize(["mcts/tic.pyx",
+                             "mcts/connect.pyx",
+                             "mcts/check.pyx"]))
+
